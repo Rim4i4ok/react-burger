6c74d973fdb32c8902c7b1468d3ff36319b2440b
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-20 15:37:23 +0300

    empty burger check

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const transformedIngredients = 
+    let transformedIngredients = 
         Object.keys(props.ingredients)
               .map(igKey => {
                   return [...Array(props.ingredients[igKey])].map((_, i) => {
                       return <BurgerIngredient key={igKey + i} type={igKey} />;
                   });
-              });
+              })
+              .reduce((arr, el) => {
+                  return arr.concat(el)
+              }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
 
     return (
         <div className={classes.Burger}>
