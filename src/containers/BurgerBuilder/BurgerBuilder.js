7d366d0519213cb7a1cc11f3ec5ba485c6f7c2ce
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-21 21:53:49 +0300

    removine ingredtient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -47,15 +47,44 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredinetHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
 
+        const updatedCounted = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };        
+        updatedIngredients[type] = updatedCounted;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
     }
 
     render () {
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0;
+        }
+
+        // {salad: true, meat: false, ...}
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredintHandler} />
+                    ingredientAdded={this.addIngredintHandler}
+                    ingredientRemoved={this.removeIngredinetHandler}
+                    disabled={disableInfo} />
             </Aux>
         );
     }
