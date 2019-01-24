34d0a555a6a1d3a3d700e4243df347f5c3a47512
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-24 22:25:08 +0300

    Modal Component Performance.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxc';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = 
-        Object.keys(props.ingredients)
+    // This could be a func component, not class. Only for debbug.
+    componentWillUpdate () {
+        console.log("[OrderSummary] WillUpdate");
+    }
+
+    render () {
+
+        const ingredientSummary = 
+        Object.keys(this.props.ingredients)
               .map(igKey => {
                   return (
                     <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                     </li>
                 );
               });  
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
